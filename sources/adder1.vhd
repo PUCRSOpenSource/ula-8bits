8d00ce6617e319c0d2f6c230ad9036f9f26680a8
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2014-09-04 11:59:18 -0300

    First commit

---
+++ sources/adder1.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity adder1 is
+    port
+    (      
+    a, b, cin :in std_logic,
+    s, cout :out std_logic
+);
+end adder1;
+architecture adder01 of adder1 is
+begin
+    s     <= a xor b xor cin;
+    cout  <= (a and b) or (cin and a) or (cin and b);
+end adder01;
---
+++ sources/adder2.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity adder2 is
+    port
+    (      
+    cin     :in std_logic,
+    s, cout :out std_logic_vector (1 downto 0);
+);
+end adder2;
+
+architecture adder02 of adder2 is
+    signal c: std_logic_vector(1 downto 0);
+begin
+    A0: entity work.Adder1 port map( cin => '0' , a => a(0), b =>b(0), cout(0), s=>s(0));
+    A1: entity work.Adder1 port map( cin => c(0), a => a(1), b =>b(1), cout(1), s=>s(1));
+    cout <= c(1);
+end adder02;
